2d4b22703e14280c48ea2138321f963d107c53ea
DavideVillani00 <villani.dado@gmail.com>
2025-04-11 13:26:18 +0200

    created login api

--- routes/api/loginRouter.js
+++ routes/api/loginRouter.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const route = express.Router();
-const USERS = require("../../fakeDb");
+const { USERS } = require("../../fakeDb");
+const jwt = require("jsonwebtoken");
 
 route.post("/", (req, res) => {
   const { userName, password } = req.body;
-  const user = USERS.filter((us) => {
+  const user = USERS.find((us) => {
     return us.userName === userName;
   });
-  if (!user) return res.status(404).json({ msg: "User not find" });
+  if (!user)
+    return res.status(404).json({ err: "UserName", msg: "User not find" });
 
   //   da inserire il bycript della verifica della password
   if (user.password !== password)
-    return res.status(404).json({ msg: "Wrong password" });
+    return res.status(404).json({ err: "Password", msg: "Wrong password" });
 
-  //   genera token
-  console.log("genero token");
+  const token = jwt.sign(
+    { userName: user.userName, role: user.role },
+    process.env.SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+
+  res.status(200).json({ token, msg: "Logged in" });
 });
+
+module.exports = route;
