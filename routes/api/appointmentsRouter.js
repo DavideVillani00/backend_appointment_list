7ac608191bbf8ed9285ec79bdadcb4b4981e9f0e
DavideVillani00 <villani.dado@gmail.com>
2025-04-13 20:16:05 +0200

    started to create appointment edit

--- routes/api/appointmentsRouter.js
+++ routes/api/appointmentsRouter.js
@@ -2,6 +2,34 @@ const express = require("express");
 const router = express.Router();
 const { APPOINTMENT_LIST } = require("../../fakeDb.js");
 
+router.post("/add", (req, res) => {
+  const { userName, date, title, time } = req.body;
+  const appointment = {
+    id: Math.random() * 1000,
+    userName,
+    title,
+    date,
+    time,
+    check: false,
+  };
+  APPOINTMENT_LIST.push(appointment);
+  res.status(201).json({ msg: "Appointment created" });
+});
+
+router.post("/edit", (req, res) => {
+  const { id, username, title, date, time, check } = req.body;
+  const appointment = APPOINTMENT_LIST.find((app) => app.id == id);
+  if (!appointment) {
+    return res.status(404).json({ msg: "Appointment not found" });
+  }
+  if (username) appointment.userName = username;
+  if (title) appointment.title = title;
+  if (date) appointment.date = date;
+  if (time) appointment.time = time;
+  if (check) appointment.check = check;
+  res.status(200).json({ msg: "Appointment updated" });
+});
+
 router.post("/search", (req, res) => {
   const { searchTitle, date, userName, check } = req.body;
 
@@ -12,12 +40,11 @@ router.post("/search", (req, res) => {
     );
   }
   if (date) {
-    console.log(date);
     result = result.filter(
       (user) => Date.parse(user.date) === Date.parse(date)
     );
   }
-  if (userName) {
+  if (userName && userName !== "All") {
     result = result.filter((user) => user.userName === userName);
   }
   if (typeof check === "boolean") {
