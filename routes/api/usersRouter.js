c83abd07a6f3826b30e22d3a23fdb9aa7ce13f67
DavideVillani00 <villani.dado@gmail.com>
2025-04-23 13:31:20 +0200

    started to create api with mysql db

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,64 +1,117 @@
 const express = require("express");
 const router = express.Router();
 const { USERS } = require("../../fakeDb.js");
+const conn = require("../../connectionDb.js");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.status(200).json(USERS);
+router.get("/", async (req, res) => {
+  try {
+    const [rows] = await conn.query("SELECT * FROM users");
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json({ msg: "Database error", err });
+  }
 });
 
-router.get("/search/:id", (req, res) => {
+router.get("/search/:id", async (req, res) => {
   const { id } = req.params;
-  const user = USERS.find((u) => u.id == id);
-  if (!user) res.status(404).json({ msg: "User not found" });
-  res.status(200).json(user);
+  try {
+    const [rows] = await conn.query("SELECT * FROM users WHERE userId = ?", [
+      id,
+    ]);
+    if (rows.length === 0) {
+      return res.status(204).json({ msg: "User not found" });
+    }
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json({ msg: "Database error", err });
+  }
 });
+// const user = USERS.find((u) => u.id == id);
+// if (!user) res.status(404).json({ msg: "User not found" });
+// res.status(200).json(user);
 
-router.post("/search", (req, res) => {
+router.post("/search", async (req, res) => {
+  //! da controllare
   const { userName, role } = req.body;
-  let filteredUser = USERS;
-  if (userName) {
-    filteredUser = filteredUser.filter((u) => u.userName === userName);
+  try {
+    const [rows] = await conn.query(
+      "SELECT * FROM users WHERE userName = ? AND role = ?",
+      [userName, role]
+    );
+    if (rows.length === 0) {
+      res.status(204).json({ msg: "Users not found", arr: rows });
+    }
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json({ msg: "Database error", err });
   }
-  if (role) {
-    filteredUser = filteredUser.filter((u) => u.role === role);
-  }
-  if (filteredUser.length === 0) {
-    return res.status(404).json({ msg: "Appointment not found", arr: [] });
-  }
-  res.status(200).json(filteredUser);
+
+  // let filteredUser = USERS;
+  // if (userName) {
+  //   filteredUser = filteredUser.filter((u) => u.userName === userName);
+  // }
+  // if (role) {
+  //   filteredUser = filteredUser.filter((u) => u.role === role);
+  // }
+  // if (filteredUser.length === 0) {
+  //   return res.status(404).json({ msg: "Appointment not found", arr: [] });
+  // }
+  // res.status(200).json(filteredUser);
 });
 
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
   const { role, userName, email, password, name, surname, company, gender } =
     req.body;
-  const usernameNotAvaible = USERS.find((u) => u.userName === userName);
-  if (usernameNotAvaible) {
-    return res
-      .status(406)
-      .json({ err: "userName", msg: "User-name is not avaible" });
+
+  try {
+    const passHash = await bcrypt.hash(password, 10);
+    await conn.query(
+      "INSERT INTO users (role, userName, email, password, firstName, lastName, gender, company) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [role, userName, email, passHash, name, surname, gender, company]
+    );
+    res.status(201).json({ msg: "User created" });
+  } catch (err) {
+    if (err.code === "ER_DUP_ENTRY") {
+      const errorTarget = err.sqlMessage.includes("userName")
+        ? "userName"
+        : "email";
+      return res
+        .status(406)
+        .json({ msg: `${errorTarget} is not avaible`, err: errorTarget });
+    }
+    res.status(500).json({ msg: "Database error:", err });
   }
 
-  const emailNotAvaible = USERS.find((u) => u.email === email);
-  if (emailNotAvaible) {
-    return res.status(406).json({ err: "email", msg: "Email is not avaible" });
-  }
-  const newUser = {
-    id: Math.random() * 1000,
-    role,
-    userName,
-    email,
-    password,
-    name,
-    surname,
-    gender,
-    company,
-  };
+  // const usernameNotAvaible = USERS.find((u) => u.userName === userName);
+  // if (usernameNotAvaible) {
+  //   return res
+  //     .status(406)
+  //     .json({ err: "userName", msg: "User-name is not avaible" });
+  // }
 
-  USERS.push(newUser);
-  res.status(201).json({ msg: "User created" });
+  // const emailNotAvaible = USERS.find((u) => u.email === email);
+  // if (emailNotAvaible) {
+  //   return res.status(406).json({ err: "email", msg: "Email is not avaible" });
+  // }
+  // const newUser = {
+  //   id: Math.random() * 1000,
+  //   role,
+  //   userName,
+  //   email,
+  //   password,
+  //   name,
+  //   surname,
+  //   gender,
+  //   company,
+  // };
+
+  // USERS.push(newUser);
+  // res.status(201).json({ msg: "User created" });
 });
 
-router.put("/edit", (req, res) => {
+router.put("/edit", async (req, res) => {
+  // !non funziona
   const {
     id,
     role,
@@ -71,43 +124,70 @@ router.put("/edit", (req, res) => {
     gender,
   } = req.body;
 
-  const user = USERS.find((u) => u.id == id);
-
-  if (!user) {
-    return res.status(404).json({ msg: "User not found" });
+  try {
+    const passHash = await bcrypt.hash(password, 10);
+    await conn.query(
+      "UPDATE users SET role = ?, userName = ?, email = ?, password = ?, firstName = ?, lastName = ?, gender = ?, company = ? WHERE userId = ?",
+      [role, userName, email, passHash, name, surname, gender, company, id]
+    );
+    res.status(201).json({ msg: "User updated" });
+  } catch (err) {
+    if (err.code === "ER_DUP_ENTRY") {
+      const errorTarget = err.sqlMessage.includes("userName")
+        ? "userName"
+        : "email";
+      return res
+        .status(406)
+        .json({ msg: `${errorTarget} is not avaible`, err: errorTarget });
+    }
+    res.status(500).json({ msg: "Database error:", err });
   }
-  const usernameNotAvaible = USERS.find((u) => u.userName === userName);
-  if (usernameNotAvaible && usernameNotAvaible.id != id) {
-    return res
-      .status(406)
-      .json({ err: "userName", msg: "User-name is not avaible" });
-  }
 
-  const emailNotAvaible = USERS.find((u) => u.email === email);
-  if (emailNotAvaible && emailNotAvaible.id != id) {
-    return res.status(406).json({ err: "email", msg: "Email is not avaible" });
-  }
+  // const user = USERS.find((u) => u.id == id);
 
-  if (role) user.role = role;
-  if (userName) user.userName = userName;
-  if (email) user.email = email;
-  if (password) user.password = password;
-  if (name) user.name = name;
-  if (surname) user.surname = surname;
-  if (gender) user.gender = gender;
-  if (company) user.company = company;
+  // if (!user) {
+  //   return res.status(404).json({ msg: "User not found" });
+  // }
+  // const usernameNotAvaible = USERS.find((u) => u.userName === userName);
+  // if (usernameNotAvaible && usernameNotAvaible.id != id) {
+  //   return res
+  //     .status(406)
+  //     .json({ err: "userName", msg: "User-name is not avaible" });
+  // }
 
-  res.status(200).json({ msg: "User updated" });
+  // const emailNotAvaible = USERS.find((u) => u.email === email);
+  // if (emailNotAvaible && emailNotAvaible.id != id) {
+  //   return res.status(406).json({ err: "email", msg: "Email is not avaible" });
+  // }
+
+  // if (role) user.role = role;
+  // if (userName) user.userName = userName;
+  // if (email) user.email = email;
+  // if (password) user.password = password;
+  // if (name) user.name = name;
+  // if (surname) user.surname = surname;
+  // if (gender) user.gender = gender;
+  // if (company) user.company = company;
+
+  // res.status(200).json({ msg: "User updated" });
 });
 
-router.delete("/delete/:id", (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
+  // !da sistemare, non mi da errore in caso di mancanza di user
   const { id } = req.params;
-  const userIndex = USERS.findIndex((u) => u.id == id);
-  if (userIndex == -1) return res.status(404).json({ msg: "User not found" });
+  try {
+    await conn.query("DELETE FROM users WHERE userId = ?", [id]);
+    res.status(200).json({ msg: "User deleted" });
+  } catch (err) {
+    res.status(500).json({ msg: "Database error:", err });
+  }
 
-  USERS.splice(userIndex, 1);
+  // const userIndex = USERS.findIndex((u) => u.id == id);
+  // if (userIndex == -1) return res.status(404).json({ msg: "User not found" });
 
-  res.status(200).json({ msg: "User deleted" });
+  // USERS.splice(userIndex, 1);
+
+  // res.status(200).json({ msg: "User deleted" });
 });
 
 module.exports = router;
